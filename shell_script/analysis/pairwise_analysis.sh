16518b0175b1eb6d0a890a21de9a4df5b588df88
momo0817 <furumomo0817@gmail.com>
2025-07-20 20:46:28 +0900

    add info

---
+++ shell_script/analysis/pairwise_analysis.sh
@@ -0,0 +1,11 @@
+python3 src/analysis/pairwise/calc_pairwise_acc.py \
+
+python3 src/analysis/pairwise/verify_checklist_pairwise.py \
+
+python3 analysis/src/pairwise/classificate_subset.py \
+
+python3 analysis/src/pairwise/ablation.py \
+
+python3 analysis/src/remove_order_sensitive_cases.py \
+
+python3 analysis/src/pairwise/analyze_ablation_checklist.py \
---
+++ shell_script/analysis/scoring_analysis.sh
@@ -0,0 +1,11 @@
+python3 src/analysis/scoring/calc_krippendoff_alpha.py \
+
+python3 src/analysis/scoring/verify_checklist_scoring.py \
+
+python3 analysis/src/scoring/classificate_subset_scoring.py \
+
+python3 analysis/src/scoring/ablation_scoring.py \
+
+python3 analysis/src/scoring/analyze_ablation_checklist_scoring.py \
+
+
